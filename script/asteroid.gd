8a6ecd0519cb9b2375352e16defac2f6c93722ef
OiFelipee <felipehellmann2006@gmail.com>
2025-11-27 17:43:57 -0300

    fiz o codigo dos arteroids e seu spawn aleatorio, junto aos timers de spawn e exclusao, as positions e os markers

--- script/asteroid.gd
+++ script/asteroid.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export var speed = 140
 @export var rotation_speed = 1.3
+@onready var timer_delete = $TimerDelete
 var start_side
 
 func _ready() -> void:
@@ -10,9 +11,20 @@ func _ready() -> void:
 	else:
 		start_side = "left"
 
-
- func _process(delta: float) -> void:
+func _process(delta: float) -> void:
 	if (start_side == "right"):
 		global_position.x -= speed*delta
+		rotation -= rotation_speed*delta
+
 	elif (start_side == "left"):
-		global_position += speed*delta
+		global_position.x += speed*delta
+		rotation += rotation_speed*delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	timer_delete.start()
+
+
+func _on_timer_delete_timeout() -> void:
+	queue_free()
+	print ("deleted")
--- script/game.gd
+++ script/game.gd
@@ -4,6 +4,9 @@ extends Node2D
 @onready var label_pongs = $Pong
 @onready var ball = $Ball
 @onready var tutorial = $Tutorial
+@onready var positions = $Positions
+var last_position 
+var asteroid_scene = preload("res://scenes/asteroid.tscn")
 
 func _process(_delta: float) -> void:
 	
@@ -21,9 +24,20 @@ func _on_hole_body_entered(_body: Node2D) -> void:
 func reload_scene():
 	get_tree().reload_current_scene()
 
-#spawner
-func _on_time_spawner_timeout() -> void:
+
+func _on_timer_spawner_timeout() -> void:
 	spawn_asteroid()
 
+
 func spawn_asteroid():
-	pass
+	if (ball.started):
+		var positions_list = positions.get_children()
+		var _spawn_position = positions_list.pick_random()
+		
+		if (_spawn_position != last_position):
+			var _asteroid_instance = asteroid_scene.instantiate()
+			_asteroid_instance.global_position =  _spawn_position.position
+			add_child(_asteroid_instance)
+			last_position = _spawn_position
+		else:
+			spawn_asteroid()
